dc1ce0adb5db64ec3c80933bda256a3c50e189ab
Mohammad Salman Ansari <mohd.salman01a@gmail.com>
2025-01-04 13:50:04 +0530

    Updated database

--- Frontend/src/utils.jsx
+++ Frontend/src/utils.jsx
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
   toast[type](message);
 };
 
-export const API_URL = "https://backend-one-phi-41.vercel.app/";
+export const API_URL = "http://localhost:8080/";
